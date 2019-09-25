79c7ff00ce4bc18ed704de480fd4706ab82bb772
aladiev.id <aladiev.id@barl.ru>
2019-09-25 19:27:34 +0300

    Polygon moving by path (TODO: Refactoring)

--- main.py
+++ main.py
@@ -67,6 +67,7 @@ class MyPolygon(object):
         self.current_points = self.points.copy()
         self.count = len(self.current_points) // 2
         self.center_figure = -1
+        self.child_offset = -1
         self.calculate_center()
         self.calculate_offset()
 
@@ -105,12 +106,15 @@ class MyPolygon(object):
         print('DELETING ' + str(id))
         print(self.points_ids)
         print(self.lines_ids)
-        self.points[id] = point[0]
-        self.points[id + self.count] = point[1]
+        self.current_points[id] = point[0]
+        self.current_points[id + self.count] = point[1]
+
+        self.points = self.current_points.copy()
 
         if self.center_figure != -1:
             canvas.delete(self.center_figure)
 
+        # Delete line to right point
         right_index = (id + 1) % self.count
 
         if right_index > id:
@@ -118,15 +122,16 @@ class MyPolygon(object):
         else:
             left, right = right_index, id
 
-        if str(left) + ' ' + str(right) in self.lines_ids: # Delete line to right point
+        if str(left) + ' ' + str(right) in self.lines_ids:
             print('DELETE FROM ' + str(left) + ' TO ' + str(right))
             canvas.delete(self.lines_ids[str(left) + ' ' + str(right)])
 
             print('DRAW FROM ' + str(left) + ' TO ' + str(right))
-            self.lines_ids[str(left) + ' ' + str(right)] = canvas.create_line(self.points[left],
-                           self.points[left + self.count],
-                           self.points[right],
-                           self.points[right + self.count])
+            self.lines_ids[str(left) + ' ' + str(right)] = canvas.create_line(self.current_points[left],
+                           self.current_points[left + self.count],
+                           self.current_points[right],
+                           self.current_points[right + self.count])
+
         # Delete line to left point
         left_index = id - 1 if id > 0 else id - 1 + self.count
 
@@ -141,17 +146,15 @@ class MyPolygon(object):
             canvas.delete(self.lines_ids[str(left2) + ' ' + str(right2)])
 
             print('DRAW FROM ' + str(left2) + ' TO ' + str(right2))
-            self.lines_ids[str(left2) + ' ' + str(right2)] = canvas.create_line(self.points[right2],
-                                                                                            self.points[right2 + self.count],
-                                                                                            self.points[left2],
-                                                                                            self.points[
+            self.lines_ids[str(left2) + ' ' + str(right2)] = canvas.create_line(self.current_points[right2],
+                                                                                            self.current_points[right2 + self.count],
+                                                                                            self.current_points[left2],
+                                                                                            self.current_points[
                                                                                                 left2 + self.count])
 
         canvas.delete(self.points_ids[id])
         self.points_ids[id] = canvas.create_oval(point[0] - 5, point[1] - 5,
                            point[0] + 5, point[1] + 5, fill="#476042")
-
-        self.current_points = self.points.copy()
 
         self.calculate_center()
         self.calculate_offset()
@@ -171,6 +174,8 @@ class MyPolygon(object):
                         self.points[i + self.count] - self.center[1]) * math.sin(angle)
             self.current_points[i + self.count] = self.center[1] + (self.points[i] - self.center[0]) * math.sin(angle) + (
                         self.points[i + self.count] - self.center[1]) * math.cos(angle)
+
+        self.move_child()
 
     def set_center(self, center):
         self.center = center.copy()
@@ -199,7 +204,7 @@ class MyPolygon(object):
             self.offset[i + self.count] = self.points[i + self.count] - self.center[1]
 
     def draw(self):
-        canvas.create_oval(self.center[0] - 3, self.center[1] - 3,
+        self.center_figure = canvas.create_oval(self.center[0] - 3, self.center[1] - 3,
                            self.center[0] + 3, self.center[1] + 3, fill="#FF0000")
 
         for i in range(0, self.count):
@@ -227,6 +232,28 @@ class MyPolygon(object):
 
         return -1
 
+    def get_child_position(self):
+        if self.child_offset != -1:
+            right_index = (self.child_offset[0] + 1) % self.count
+            left_index = self.child_offset[0]
+
+            x = self.current_points[left_index] + (self.current_points[right_index] - self.current_points[left_index]) * self.child_offset[1] / 100
+            y = self.current_points[left_index + self.count] + (self.current_points[right_index + self.count] - self.current_points[left_index + self.count]) * self.child_offset[1] / 100
+
+            return [x, y]
+
+        self.child_offset = [0, 0]
+        return [self.current_points[0], self.current_points[self.count]]
+
+    def move_child(self):
+        if self.child_offset != -1:
+            right_index = (self.child_offset[0] + 1) % self.count
+
+            if self.child_offset[1] + 0.3 < 100:
+                self.child_offset[1] += 0.3
+            else:
+                self.child_offset = [right_index, 0]
+
 def on_click_canvas(point):
     global state, polygons, run, moving_point
     print(state)
@@ -239,6 +266,11 @@ def on_click_canvas(point):
             if polygon_id == 0 and point_id == 0 and state == SOME:
                 polygons[0].last_vertex()
                 set_state(LAST)
+                return
+            if polygon_id == 1 and point_id == 0 and state == SOME_SECOND:
+                polygons[1].last_vertex()
+                polygons[1].set_center(polygons[0].get_child_position())
+                set_state(END)
                 return
             else:
                 moving_point = [polygon_id, point_id]
@@ -258,16 +290,14 @@ def on_click_canvas(point):
             set_state(SOME)
         else:
             if state == SOME:
-                if polygon_id > -1:
-                    if get_figure(point) == 0:
-                        polygons[0].last_vertex()
-                        set_state(LAST)
-                        run = True    
-                else:
-                    polygons[0].add_vertex(point)
+                polygons[0].add_vertex(point)
             else:
                 if state == LAST:
-                    pass
+                    polygons.append(MyPolygon(point))
+                    set_state(SOME_SECOND)
+                else:
+                    if state == SOME_SECOND:
+                        polygons[1].add_vertex(point)
 
 def callback(event):
     print("clicked at", event.x, event.y)
@@ -286,33 +316,16 @@ stop_button.pack()
 
 time = 0
 
-# for i in range(0, 12):
-#     phi = i * math.pi / 6
-#
-#     x = c_x + radius * math.cos(phi + time)
-#     y = c_y + radius * math.sin(phi + time)
-#
-#     points = [x - square_size / 2,
-#                    x,
-#                    x + square_size / 2,
-#                    x + square_size * 2,
-#                    x - square_size / 4,
-#                    y - square_size / 2,
-#                    y - square_size,
-#                    y - square_size / 2,
-#                    y + square_size / 2,
-#                    y + square_size / 2]
-#
-#     polygons.append(MyPolygon(points))
-#
-#     polygons[i].draw()
-
 def draw():
     global time
     if run:
         canvas.delete('all')
-        polygons[0].rotation(-1 * time)
+        polygons[0].rotation( time)
         polygons[0].draw()
+
+        polygons[1].set_center(polygons[0].get_child_position())
+        polygons[1].rotation(-1 * time)
+        polygons[1].draw()
 
         time += math.pi / 180
     root.after(10, draw)
