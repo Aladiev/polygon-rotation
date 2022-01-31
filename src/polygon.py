4b53bbb72239b0f83c7c63b5e427ab8326625c9f
aladiev.id <aladiev.id@barl.ru>
2022-01-31 17:12:56 +0300

    Refactored and minimize code

--- src/polygon.py
+++ src/polygon.py
@@ -1,9 +1,12 @@
 from config import *
 import math
 
-class MyPolygon(object):
+
+class Polygon(object):
     def __init__(self, points, canvas):
         self.canvas = canvas
+
+        # Copy points
 
         self.points, self.initial_points = [], []
 
@@ -11,33 +14,46 @@ class MyPolygon(object):
             self.initial_points.append(point.copy())
             self.points.append(point.copy())
 
-        self.lines_ids = []
+        self.angle, self.perimeter, self.child_offset, self.lines_ids = 0, 0, [0, 0], []
+        self.center, self.perimeter, self.offset = [0, 0], 0, []
 
-        self.draw_lines()
-        self.draw_points()
+        # Calculating center
 
-        self.angle = 0
+        for i in range(len(self.initial_points)):
+            self.center[0] += self.initial_points[i][0]
+            self.center[1] += self.initial_points[i][1]
 
-        self.center = 0
+        self.center = [self.center[0] // len(self.initial_points), self.center[1] // len(self.initial_points)]
 
-        self.child_offset = [0, 0]
+        # Calculating perimeter and offset
 
-        self.perimeter = 0
+        for i in range(len(self.initial_points)):
+            self.offset.append([
+                self.initial_points[i][0] - self.center[0],
+                self.initial_points[i][1] - self.center[1]
+            ])
 
-        self.calculate_center()
-        self.calculate_offset()
-        self.calculate_perimeter()
+            right_index = (i + 1) % len(self.initial_points)
+
+            dx = self.initial_points[i][0] - self.initial_points[right_index][0]
+            dy = self.initial_points[i][1] - self.initial_points[right_index][1]
+
+            self.perimeter += math.sqrt(dx ** 2 + dy ** 2)
+
+        self.draw()
 
     def rotation(self, angle):
         self.angle = angle
-        for i in range(len(self.points)):
-            self.points[i][0] = self.center[0] + (self.initial_points[i][0] - self.center[0]) * math.cos(angle) - (
-                    self.initial_points[i][1] - self.center[1]) * math.sin(angle)
-            self.points[i][1] = self.center[1] + (self.initial_points[i][0] - self.center[0]) * math.sin(
-                angle) + (self.initial_points[i][1] - self.center[1]) * math.cos(angle)
 
-        self.draw_lines()
-        self.draw_points()
+        for i in range(len(self.points)):
+            self.points[i][0] = self.center[0] \
+                + (self.initial_points[i][0] - self.center[0]) * math.cos(angle) \
+                - (self.initial_points[i][1] - self.center[1]) * math.sin(angle)
+            self.points[i][1] = self.center[1] \
+                + (self.initial_points[i][0] - self.center[0]) * math.sin(angle) \
+                + (self.initial_points[i][1] - self.center[1]) * math.cos(angle)
+
+        self.draw()
         self.move_child()
 
     def set_center(self, center):
@@ -47,34 +63,7 @@ class MyPolygon(object):
             self.initial_points[i][0] = self.center[0] + self.offset[i][0]
             self.initial_points[i][1] = self.center[1] + self.offset[i][1]
 
-    def calculate_center(self):
-        summa_x = 0
-        summa_y = 0
-
-        for i in range(len(self.points)):
-            summa_x += self.points[i][0]
-            summa_y += self.points[i][1]
-
-        self.center = [summa_x // len(self.points), summa_y // len(self.points)]
-
-    def calculate_offset(self):
-        self.offset = []
-
-        for point in self.initial_points:
-            self.offset.append([point[0] - self.center[0], point[1] - self.center[1]])
-
-    def calculate_perimeter(self):
-        self.perimeter = 0
-
-        for i in range(len(self.points)):
-            right_index = (i + 1) % len(self.points)
-
-            dx = self.points[i][0] - self.points[right_index][0]
-            dy = self.points[i][1] - self.points[right_index][1]
-
-            self.perimeter += math.sqrt(dx ** 2 + dy ** 2)
-
-    def draw_lines(self):
+    def draw(self):
         for line in self.lines_ids:
             self.canvas.delete(line)
 
@@ -86,7 +75,6 @@ class MyPolygon(object):
                                                           self.points[(point_id + 1) % len(self.points)][0],
                                                           self.points[(point_id + 1) % len(self.points)][1]))
 
-    def draw_points(self):
         for point in self.points:
             self.canvas.delete(point[3])
 
@@ -99,34 +87,30 @@ class MyPolygon(object):
             )
 
     def get_child_position(self):
-        if self.child_offset != -1:
-            right_index = (self.child_offset[0] + 1) % len(self.points)
-            left_index = self.child_offset[0]
+        right_index = (self.child_offset[0] + 1) % len(self.points)
+        left_index = self.child_offset[0]
 
-            dx = self.points[right_index][0] - self.points[left_index][0]
-            dy = self.points[right_index][1] - self.points[left_index][1]
+        dx = self.points[right_index][0] - self.points[left_index][0]
+        dy = self.points[right_index][1] - self.points[left_index][1]
 
-            x = self.points[left_index][0] + dx * self.child_offset[1] / math.sqrt(dx ** 2 + dy ** 2)
-            y = self.points[left_index][1] + dy * self.child_offset[1] / math.sqrt(dx ** 2 + dy ** 2)
+        x = self.points[left_index][0] + dx * self.child_offset[1] / math.sqrt(dx ** 2 + dy ** 2)
+        y = self.points[left_index][1] + dy * self.child_offset[1] / math.sqrt(dx ** 2 + dy ** 2)
 
-            return [x, y]
-
-        self.child_offset = [0, 0]
-        return self.points[0].copy()
+        return [x, y]
 
     def move_child(self):
-        if self.child_offset != -1:
-            right_index = (self.child_offset[0] + 1) % len(self.points)
+        right_index = (self.child_offset[0] + 1) % len(self.points)
 
-            dx = self.points[self.child_offset[0]][0] - self.points[right_index][0]
-            dy = self.points[self.child_offset[0]][1] - self.points[right_index][1]
+        dx = self.points[self.child_offset[0]][0] - self.points[right_index][0]
+        dy = self.points[self.child_offset[0]][1] - self.points[right_index][1]
 
-            if math.sqrt(dx ** 2 + dy ** 2) < (self.child_offset[1] + 0.001 * self.perimeter):
-                movement = 0.001 * self.perimeter + self.child_offset[1] - math.sqrt(dx ** 2 + dy ** 2)
-                self.child_offset = [right_index, movement]
-                self.calculate_child_offset()
-            else:
-                self.child_offset[1] += 0.001 * self.perimeter
+        if math.sqrt(dx ** 2 + dy ** 2) < (self.child_offset[1] + 0.001 * self.perimeter):
+            movement = 0.001 * self.perimeter + self.child_offset[1] - math.sqrt(dx ** 2 + dy ** 2)
+
+            self.child_offset = [right_index, movement]
+            self.calculate_child_offset()
+        else:
+            self.child_offset[1] += 0.001 * self.perimeter
 
     def calculate_child_offset(self):
         right_index = (self.child_offset[0] + 1) % len(self.points)
@@ -136,16 +120,6 @@ class MyPolygon(object):
 
         if self.child_offset[1] > math.sqrt(dx ** 2 + dy ** 2):
             movement = self.child_offset[1] - math.sqrt(dx ** 2 + dy ** 2)
+
             self.child_offset = [right_index, movement]
             self.calculate_child_offset()
-
-    def calculate_perimeter(self):
-        self.perimeter = 0
-
-        for i in range(len(self.initial_points)):
-            right_index = (i + 1) % len(self.initial_points)
-
-            dx = self.initial_points[i][0] - self.initial_points[right_index][0]
-            dy = self.initial_points[i][1] - self.initial_points[right_index][1]
-
-            self.perimeter += math.sqrt(dx ** 2 + dy ** 2)
